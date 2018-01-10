3f17caaa2fa875ce604252ab2593a0adb52dfb2f
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-10 23:16:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(num)
+=begin  sq_numbers = (2..num).to_a
+  sq_numbers.each do |sq|
+    x = sq*sq
+    break if x > num
+  end
+
+  prime_numbers = (2..sq_numbers).to_a
+  prime_numbers.each do |y|
+    num%y == 0
+  end
+
+  if prime_numbers == nil
+    true
+  else
+    false
+  end
+=end
+
+  if num > 1
+    true
+  else
+    false
+  end
+end
