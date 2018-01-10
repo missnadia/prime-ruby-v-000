256a006aecaad11dd1bfc7f3bcd1b713e3b6e8aa
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-10 23:55:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(num)
-=begin  sq_numbers = (2..num).to_a
+ sq_numbers = (1..num).to_a
   sq_numbers.each do |sq|
-    x = sq*sq
-    break if x > num
+    sq*sq
+    break if sq > num
   end
 
   prime_numbers = (2..sq_numbers).to_a
@@ -11,13 +11,6 @@ def prime?(num)
   end
 
   if prime_numbers == nil
-    true
-  else
-    false
-  end
-=end
-
-  if num > 1 && (num%2==0) || (num%3==0)
     true
   else
     false
